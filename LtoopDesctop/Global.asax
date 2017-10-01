faeb9cea6c8fb8d93fba1dae721e8bb1380f52de
DELL <DELL@DELL->
2017-10-01 23:19:59 +0300

    Добавьте файлы проекта.

---
+++ LtoopDesctop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LtoopDesctop.MvcApplication" Language="C#" %>
---
+++ LtoopDesctop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LtoopDesctop.Startup))]
+namespace LtoopDesctop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
